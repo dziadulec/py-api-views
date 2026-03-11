e6562306117a9dfa96af81f30953764f3554389a
Marek Drządzewski <dzadzumaster@gmail.com>
2026-03-11 14:29:53 +0100

    feat: configure urls and routers for api views

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,49 @@
-from django.urls import path
+from os import name
 
-from cinema.views import movie_list, movie_detail
+from django.urls import path, include
+from rest_framework import routers
+
+from cinema.views import (
+    MovieViewSet,
+    CinemaHallViewSet,
+    ActorList, ActorDetail,
+    GenreList, GenreDetail
+)
+
+router = routers.DefaultRouter()
+router.register("movies", MovieViewSet)
+router.register("cinema_halls", CinemaHallViewSet)
 
 urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path(
+        "actors/",
+        ActorList.as_view(),
+        name="actor_list"
+    ),
+    path(
+        "actors/<int:pk>/",
+        ActorDetail.as_view(),
+        name="actor_detail"
+    ),
+    path(
+        "genres/",
+        GenreList.as_view(),
+        name="genre_list"
+    ),
+    path(
+        "genres/<int:pk>/",
+        GenreDetail.as_view(), name="genre_detail"
+    ),
+
+
+    path(
+        "",
+        include(router.urls)
+    ),
+
+
+
+
 ]
 
 app_name = "cinema"
