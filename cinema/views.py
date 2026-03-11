2cb1b0e5c696dc3b9649e5709bc99083e94008c6
Marek Drządzewski <dzadzumaster@gmail.com>
2026-03-11 14:59:57 +0100

    fix: delete @staticmethod decorator add: partial=True to patch method of GenreDetail class

--- cinema/views.py
+++ cinema/views.py
@@ -85,18 +85,16 @@ class GenreList(APIView):
 class GenreDetail(
     APIView
 ):
-    @staticmethod
     def get_object(self, pk):
-        genre = get_object_or_404(Genre, pk=pk)
-        return genre
+        return get_object_or_404(Genre, pk=pk)
 
     def get(self, request, pk):
-        genre = self.get_object(Genre, pk=pk)
+        genre = self.get_object(pk)
         serializer = GenreSerializer(genre)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def put(self, request, pk):
-        genre = self.get_object(Genre, pk=pk)
+        genre = self.get_object(pk)
         serializer = GenreSerializer(genre, data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -104,14 +102,14 @@ class GenreDetail(
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def patch(self, request, pk):
-        genre = self.get_object(Genre, pk=pk)
-        serializer = GenreSerializer(genre, data=request.data)
+        genre = self.get_object(pk)
+        serializer = GenreSerializer(genre, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
-        genre = self.get_object(Genre, pk=pk)
+        genre = self.get_object(pk)
         genre.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
